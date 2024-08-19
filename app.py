9ba7177ae4cf32a6fd4a676960614b8965a9b04b
gabsdsm1 <gabrillysilva2@gmail.com>
2024-08-19 15:59:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-app.run()
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/opcaoc')
+def opcaoc():
+    return render_template("opcaoc.html")
+
+@app.route('/cadastroempresa')
+def cadastroempresa():
+    return render_template("cadastroempresa.html")
+
+@app.route('/telainicial')
+def telainicial():
+    return render_template("telainicial.html")
+
+@app.route('/recuperar-senha')
+def recuperarsenha():
+    return render_template("recuperar-senha.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
